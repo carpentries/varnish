55b9b9cabeb2797d97b0d84b2819eee81b875018
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-21 17:24:12 -0800

    add source files and builder

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,5 @@
+#= require vendor/jquery/jquery
+#= require vendor/bootstrap/bootstrap
+#= require vendor/jquery-visible/jquery.visible
+#= require vendor/feather-icons/feather
+#= require custom/menu 
---
+++ squash-a-script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p inst/pkgdown/assets/assets/
+vend=source/javascripts/vendor
+cust=source/javascripts/custom
+
+uglifyjs ${vend}/jquery/jquery.js \
+  ${vend}/bootstrap/bootstrap.js \
+  ${vend}/jquery-visible/jquery.visible.js \
+  ${vend}/feather-icons/feather.js \
+  ${cust}/menu.js \
+  --compress \
+  --output inst/pkgdown/assets/assets/scripts.js
+

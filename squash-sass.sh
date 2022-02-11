4658636e56654b163bfc9da7bd28f6159fc6f28d
Saba <saba.ferdous.13@ucl.ac.uk>
2022-02-11 21:07:08 +0000

    Update squash-sass.sh

--- squash-sass.sh
+++ squash-sass.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "building assets"
 mkdir -p inst/pkgdown/assets/assets/
 cp -r source/fonts/ inst/pkgdown/assets/assets/fonts/
 cp -r source/images/ inst/pkgdown/assets/assets/images/

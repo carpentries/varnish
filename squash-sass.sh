fe0635ff0ee0ba7a78f9bfbf5d84aca82d7847d3
Zhian N. Kamvar <zkamvar@gmail.com>
2023-08-31 11:47:51 -0700

    fix squash sass to not create new folders

--- squash-sass.sh
+++ squash-sass.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p inst/pkgdown/assets/assets/
-cp -r source/fonts/ inst/pkgdown/assets/assets/fonts/
-cp -r source/images/ inst/pkgdown/assets/assets/images/
+mkdir -p inst/pkgdown/assets/assets/{fonts,images}
+cp -r source/fonts/* inst/pkgdown/assets/assets/fonts/
+cp -r source/images/* inst/pkgdown/assets/assets/images/
 
 node_modules/.bin/sass -s compressed \
   source/stylesheets/styles.css.scss \
